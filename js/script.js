b6202a40b0f8072fe9bd1e91e45311d27ff880bc
Md. Abu Bakar Siddique Sadi <sadiakash1309@gmail.com>
2025-02-03 18:49:11 +0600

    Initial

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const themeToggleBtn = document.getElementById("theme-toggle");
+const currentTheme = localStorage.getItem("theme");
+
+// Apply the saved theme on page load
+if (currentTheme === "dark") {
+  document.body.classList.add("dark-mode");
+}
+
+themeToggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  // Save theme preference in localStorage
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
